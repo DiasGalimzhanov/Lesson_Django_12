08a0f26668527754d112cc891dcc5dd2530412a7
DiasGalimzhanov <126086058+DiasGalimzhanov@users.noreply.github.com>
2024-03-06 16:15:21 +0600

    homework 12

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,11 @@ class Forms(forms.ModelForm):
         model = Person
         fields = ['name', 'birth_date', 'work_place', 'salary', 'gender']
         widgets = {
-            'birth_date' : forms.DateInput(attrs={'type': 'date'}),
+            'name' : forms.TextInput(attrs={'class': 'form-control','id': 'validationCustom01', 'required': 'true', 'placeholder': 'Name'}),
+            'work_place' : forms.TextInput(attrs={'class': 'form-control','id': 'validationCustom01', 'required': 'true', 'placeholder': 'Work place'}),
+            'salary' : forms.NumberInput(attrs={'class': 'form-control','id': 'validationCustom01', 'required': 'true', 'placeholder': 'Salary'}),
+            'gender' : forms.Select(attrs={'class': 'form-control','id': 'validationCustom01', 'required': 'true', 'placeholder': 'Gender'}),
+            'birth_date' : forms.DateInput(attrs={'type': 'date', 'class': 'form-control','id': 'validationCustom01', 'required': 'true', 'placeholder': 'Birth date'}),
         }
 
 
